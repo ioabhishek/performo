5fbf6723c5d1eee040cf7ac4eb4b517c10b7fa3b
Abhishek Sharma <abhishek.sharma@byteme.co.in>
2023-10-18 19:59:39 +0530

    latest changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google';
-import Sidebar from '@/components/sideBar/Sidebar';
+import Sidebar from '@/components/sideBarMenu/Sidebarm';
 
 const inter = Inter({ subsets: ['latin'] })
 
