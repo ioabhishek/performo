4cf2aff91bed3b4007bc96fe989db4b218194f34
Atishay Jain <115861964+supersonicspeedjj@users.noreply.github.com>
2023-10-25 11:06:18 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
 import Main from '@/components/mainContainer/Main'
 
+import styles from './page.module.css'
+
+import Signin from '@/components/signInPage/Signin'
+import Signup from '@/components/signInPage/Signup'
 export default function Home() {
   return (
-    <main>
-      <Main/>
-    </main>
+    
+
+    <div className={styles.testing}>
+    <Signup/>
+   
+    </div>
+   
   )
 }
---
+++ src/components/signInPage/Signup.js
@@ -0,0 +1,67 @@
+'use client'
+import React from 'react'
+import styles from './signin.module.css'
+import Link from 'next/link'
+function Signup() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const email = formData.get("signupemail");
+        const username = formData.get("signupusername");
+        const password = formData.get("signuppassword");
+        console.log(email, password,username);
+      };
+  return (
+    <>
+       <div className={styles.signupbox}>
+       <form  onSubmit={handleSubmit}>
+       <h4>Username</h4>
+       <input
+          type="text"
+          placeholder="choose a username"
+          name="signupusername"
+          className={styles.textfield}
+        />
+        <h4>email</h4>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          name="signupemail"
+          className={styles.textfield}
+        />
+        <h4>password</h4>
+        <input
+          type="password"
+          placeholder="Enter your password"
+          name="signuppassword"
+          className={styles.textfield}
+        />
+         <h4>re-enter you password</h4>
+        <input
+          type="password"
+          placeholder="re-enter your password"
+          name="signuppassword"
+          className={styles.textfield}
+        />
+        <button type="submit" className={styles.signinbtn}>
+          Sign Up
+        </button>
+        <div className={styles.break}>
+          <p className={styles.para}>or</p>
+        </div>
+        <button type="submit" className={styles.signinbtn}>
+          Sign Up with Google
+        </button>
+        </form>
+        <h5 className={styles.signup}>
+          Already have an account {" "} 
+          <Link href="/signin" style={{ color: "blue" }}>
+            sign in
+          </Link>
+        </h5>
+       </div>
+    </>
+  )
+}
+
+export default Signup
