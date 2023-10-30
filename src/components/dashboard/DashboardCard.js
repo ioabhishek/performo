71fbb4af3ab91fed397c27e055e02627d9c8e6c2
Abhishek Sharma <akstenx@gmail.com>
2023-10-30 18:23:52 +0530

    position fixed

--- src/components/dashboard/DashboardCard.js
+++ src/components/dashboard/DashboardCard.js
@@ -4,8 +4,8 @@ import styles from './dashboard.module.css'
 function DashboardCard({rankcount, rank}) {
   return (
     <div className={styles.main_card}>
-      <span>TOP {rankcount}</span>
-      <span>{rank}</span>
+      <span>TOP {rank}</span>
+      <span>{rankcount}</span>
     </div>
   )
 }
