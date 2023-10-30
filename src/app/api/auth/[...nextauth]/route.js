cc7375979791544c0f6d3481c839fef3eed3695b
Abhishek Sharma <akstenx@gmail.com>
2023-10-30 06:44:04 +0530

    added db post in next-auth callback

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,4 @@
 import NextAuth from "next-auth";
-import { authOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 const handler = NextAuth({
     providers: [
@@ -8,6 +7,28 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
     ],
+    callbacks: {
+        async signIn({ user, account}) {
+            if(account.provider === 'google') {
+                const {email, name, image, subscriber} = user;
+                try {
+                    const res = await fetch('http://localhost:3000/api/user', {
+                        method: "POST",
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({email, name, image, subscriber})
+                    })
+                    if(res.ok) {
+                        return user;
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+            return user;
+        }
+    }
 });
 
 export { handler as GET, handler as POST };
