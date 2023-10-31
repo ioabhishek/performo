af030c48db867ddb41c9720b14943424270b125f
Abhishek Sharma <akstenx@gmail.com>
2023-10-31 22:13:28 +0530

    removed access flickring from dashboard

--- src/components/getAccess/GetAccess.js
+++ src/components/getAccess/GetAccess.js
@@ -1,12 +1,16 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Link from 'next/link';
 import styles from './getAccess.module.css';
 import { ToastContainer, toast } from 'react-toastify';
 import { PulseLoader } from "react-spinners";
+import { useSession } from "next-auth/react";
+import { useAccess } from "@/context/accessContext";
 
 const GetAccess = () => {
    const [email, setEmail] = useState('');
+   const {status, data:session} = useSession();
+   const { accessStatus, checkAccess } = useAccess();
 
    const handleEmailChange = (e) => {
       setEmail(e.target.value)
@@ -41,16 +45,36 @@ const GetAccess = () => {
       setEmail('')
    }
 
-   return (
-      <div className={styles.access_wrap}>
-         <div className={styles.access_text}>You don&apos;t have access to use this feature. <br /> Request by entering your email</div>
-         <form action="" onSubmit={(e) => handleSubmit(e)}>
-            <input type="email" placeholder='Enter your email...' value={email} onChange={handleEmailChange}/>
-            <button type='submit' >Request</button>
-         </form>
-         <Link href="/login">Back to Login</Link>
-      </div>
-   )
+   useEffect(() => {
+      if (status === "authenticated" && accessStatus === "checking") {
+         const userEmail = session.user.email;
+         checkAccess(userEmail);
+      }
+   }, [session, accessStatus, checkAccess]);
+
+   if (accessStatus === "authenticated") {
+      return (
+         <div className='loading_wrap'>
+            <PulseLoader
+               color="#696CFF"
+               size={20}
+               data-textid="Loader"
+            />
+         </div>
+      )
+   } else if (accessStatus === "unauthenticated") {
+      return (
+         <div className={styles.access_wrap}>
+            <div className={styles.access_text}>You don&apos;t have access to use this feature. <br /> Request by entering your email</div>
+            <form action="" onSubmit={(e) => handleSubmit(e)}>
+               <input type="email" placeholder='Enter your email...' value={email} onChange={handleEmailChange}/>
+               <button type='submit' >Request</button>
+            </form>
+            <Link href="/login">Back to Login</Link>
+         </div>
+      )
+   }
+
 }
 
 export default GetAccess
