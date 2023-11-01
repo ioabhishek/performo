5f7e98b7a5cbe5a33a2fc05aba31da4f324d9bfc
Abhishek Sharma <akstenx@gmail.com>
2023-11-01 20:51:47 +0530

    changed article fetch to only if its selected

--- src/components/compareGrid/CompareWrap.js
+++ src/components/compareGrid/CompareWrap.js
@@ -9,53 +9,55 @@ const CompareWrap = ({ publisher, publisherid, categoryid, selected, searchInput
    const [showCount, setShowCount] = useState(10);
 
    useEffect(() => {
-      const fetchArticles = async () => {
-         try {
-            const startTime = performance.now();
-            let apiEndpoint = 'https://performo.in/api/all_article.php'
-
-            if (searchInput) {
-               apiEndpoint = 'https://performo.in/api/search_keyword.php'
+      if(selected) {
+         const fetchArticles = async () => {
+            try {
+               const startTime = performance.now();
+               let apiEndpoint = 'https://performo.in/api/all_article.php'
+   
+               if (searchInput) {
+                  apiEndpoint = 'https://performo.in/api/search_keyword.php'
+               }
+   
+               let json;
+   
+               if(searchInput) {
+                  const data = await fetch(apiEndpoint, {
+                     method: 'POST',
+                     headers: {
+                        Authorization: 'Bearer 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+                     },
+                     body: new URLSearchParams({publisher_id : publisherid, category_id : categoryid, keywords : searchInput, })
+                  });
+                  json = await data.json();
+               } else {
+                  const data = await fetch(apiEndpoint, {
+                     method: 'POST',
+                     headers: {
+                        Authorization: 'Bearer 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+                     },
+                     body: new URLSearchParams({publisher_id : publisherid, category_id : categoryid, page_num : 10, })
+                  });
+                  json = await data.json();
+               }
+   
+               const endTime = performance.now();
+               setLoadingTime(endTime - startTime);
+   
+               if (Array.isArray(json) && json.length === 0) {
+                  setArticles([]);
+               } else {
+                  setArticles(json);
+               }
+            } catch (error) {
+               // Handle the error as needed
+               console.error('An error occurred while fetching data:', error);
             }
-
-            let json;
-
-            if(searchInput) {
-               const data = await fetch(apiEndpoint, {
-                  method: 'POST',
-                  headers: {
-                     Authorization: 'Bearer 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-                  },
-                  body: new URLSearchParams({publisher_id : publisherid, category_id : categoryid, keywords : searchInput, })
-               });
-               json = await data.json();
-            } else {
-               const data = await fetch(apiEndpoint, {
-                  method: 'POST',
-                  headers: {
-                     Authorization: 'Bearer 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-                  },
-                  body: new URLSearchParams({publisher_id : publisherid, category_id : categoryid, page_num : 10, })
-               });
-               json = await data.json();
-            }
-
-            const endTime = performance.now();
-            setLoadingTime(endTime - startTime);
-
-            if (Array.isArray(json) && json.length === 0) {
-               setArticles([]);
-            } else {
-               setArticles(json);
-            }
-         } catch (error) {
-            // Handle the error as needed
-            console.error('An error occurred while fetching data:', error);
-         }
-      };
-
-      fetchArticles();
-   }, [searchInput, publisherid, categoryid]);
+         };
+   
+         fetchArticles();
+      }
+   }, [selected, searchInput, publisherid, categoryid]);
 
    const loadMoreArticles = () => {
       setShowCount(prevShowCount => prevShowCount + 10);
