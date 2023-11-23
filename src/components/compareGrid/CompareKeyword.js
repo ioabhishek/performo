51206d7a9518cf369124789b63db90f2fb64efea
Abhishek Sharma <akstenx@gmail.com>
2023-11-23 18:06:19 +0530

    article keyword fixes

--- src/components/compareGrid/CompareKeyword.js
+++ src/components/compareGrid/CompareKeyword.js
@@ -1,61 +1,78 @@
-import React, {useState, useEffect} from 'react'
-import styles from './compareGrid.module.css';
+import React, { useState, useEffect } from "react";
+import styles from "./compareGrid.module.css";
 import { PulseLoader } from "react-spinners";
 
-const CompareKeyword = ({id}) => {
+const CompareKeyword = ({ id }) => {
    const [keywordData, setKeywordData] = useState([]);
-   const [status, setStatus] = useState('loading');
+   const [status, setStatus] = useState("loading");
 
    useEffect(() => {
       const fetchkw = async () => {
          try {
-            const data = await fetch('https://performo.in/api/article_keywords.php', {
-               method: 'POST',
-               headers: {
-                  Authorization: 'Bearer 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-               },
-               body: new URLSearchParams({ article_id: id })
-            });
-            const json = await data.json();
-            setKeywordData(json);
-            setStatus(json.length === 0 ? 'false' : 'true');
-            // json.length === 0 ? setStatus("false") : setStatus("true")
+            const data = await fetch(
+               "https://performo.in/api/article_keywords.php",
+               {
+                  method: "POST",
+                  headers: {
+                     Authorization:
+                        "Bearer 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                  },
+                  body: new URLSearchParams({ article_id: id }),
+               }
+            );
+
+            console.log("Response status:", data.status);
+
+            if (!data.ok) {
+               throw new Error(`HTTP error! Status: ${data.status}`);
+            }
+
+            const contentType = data.headers.get("content-type");
+
+            // Choose one method based on the content type
+            if (contentType && contentType.includes("application/json")) {
+               const json = await data.json();
+               console.log("Parsed JSON:", json);
+               setKeywordData(json);
+               setStatus(json.length === 0 ? "false" : "true");
+            } else {
+               const textResponse = await data.text();
+               console.log("Text Response:", textResponse);
+               // Handle the text response accordingly
+            }
          } catch (error) {
-            // console.error("An error occurred:", error);
+            console.error("An error occurred:", error);
+            setStatus("false");
+            setKeywordData([]);
          }
       };
-    
+
       fetchkw();
    }, [id]);
 
-   if(status === "loading") {
+   console.log(keywordData, status);
+
+   if (status === "loading") {
       return (
          <div className={styles.compare_tab_itm}>
-            <PulseLoader
-               color="#696CFF"
-               size={10}
-               data-textid="Loader"
-            />
+            <PulseLoader color="#696CFF" size={10} data-textid="Loader" />
          </div>
-      ); 
+      );
    } else if (status === "false") {
       return (
          <div className={styles.compare_tab_itm}>
-            <span className='loading_text'>No data...</span>
+            <span className="loading_text">No data...</span>
          </div>
-      ); 
-   } else return (
-      <div className={styles.compare_tab_itm}>
-         {
-            keywordData?.map((keyword) => (
-               <>
-                  <span key={keyword.keyword_name}>#{keyword?.keyword_name}</span>
-               </>
-               
-            ))
-         }
-      </div>
-   )
-}
+      );
+   } else {
+      return (
+         <div className={styles.compare_tab_itm}>
+            {keywordData?.map((keyword, index) => (
+               <span key={index}>#{keyword?.keyword_name}</span>
+            ))}
+         </div>
+      );
+   }
+};
 
-export default CompareKeyword
+export default CompareKeyword;
