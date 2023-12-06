ccb87266d9b9bb68004765b548d11f2b3a3d35c7
Atishay Jain <115861964+supersonicspeedjj@users.noreply.github.com>
2023-12-06 18:02:22 +0530

    changes

--- src/components/dashboard/DashboardCardRank.js
+++ src/components/dashboard/DashboardCardRank.js
@@ -29,7 +29,7 @@ const DashboardCardRank = ({startDate, endDate, categoryId,counter}) => {
         <span className={styles.dcard_ttl}>TOP 1</span>
         {positions.length > 0 ? (
           positions.map((position) => (
-            position.rank === "1" ? <span key={position}>{position.rankcount}</span> : <span key={position}>...</span>
+            position.rank === "1" ? <span key={position}>{position.rankcount}</span> :<></>
           ))
         ) : (
           <div className={styles.dcard_wrap}>
@@ -42,7 +42,7 @@ const DashboardCardRank = ({startDate, endDate, categoryId,counter}) => {
         <span className={styles.dcard_ttl}>TOP 2</span>
         {positions.length > 0 ? (
           positions.map((position) => (
-            position.rank === "2" ? <span key={position}>{position.rankcount}</span> : <span key={position}>...</span>
+            position.rank === "2" ? <span key={position}>{position.rankcount}</span> : <></>
           ))
         ) : (
           <div className={styles.dcard_wrap}>
@@ -55,7 +55,7 @@ const DashboardCardRank = ({startDate, endDate, categoryId,counter}) => {
         <span className={styles.dcard_ttl}>TOP 3</span>
         {positions.length > 0 ? (
           positions.map((position) => (
-            position.rank === "3" ? <span key={position}>{position.rankcount}</span> : <span key={position}>...</span>
+            position.rank === "3" ? <span key={position}>{position.rankcount}</span> : <></>
           ))
         ) : (
           <div className={styles.dcard_wrap}>
